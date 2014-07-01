f7e39c7a6ac36403ce44e4aa6d4aae4c2404a7c0
Matt Zabriskie <mzabriskie@gmail.com>
2014-07-01 00:05:40 -0600

    Adding state backed by localStorage

--- src/state.js
+++ src/state.js
@@ -1,5 +1,40 @@
-var State = {
-	init: function (key) {}
-};
+(function () {
+	var key = 'assess';
+	var State = {
+		init: function () {
+			this.data = JSON.parse(localStorage.getItem(key));
+		},
 
-module.exports = State;
+		sync: function () {
+			localStorage.setItem(key, JSON.stringify(this.data));
+		},
+
+		setLapsedTime: function (lapsed) {
+			this.data.lapsed = lapsed;
+			this.sync();
+		},
+
+		getLapsedTime: function () {
+			return this.data.lapsed;
+		},
+
+		getQuestions: function () {
+			return this.data.questions;
+		},
+
+		setQuestion: function (question) {
+			this.data.questions[question.ID - 1] = question;
+			this.sync();
+		},
+
+		getQuestion: function (ID) {
+			return this.data.questions[ID - 1];
+		}
+	};
+
+	if (typeof module !== 'undefined') {
+		module.exports = State;
+	} else {
+		this.State = State;
+	}
+}).call(this);
---
+++ test/jasmine/state.spec.js
@@ -0,0 +1,62 @@
+describe('assess.js', function () {
+	var data = {
+		lapsed: 37,
+		questions: [
+			{
+				ID: 1,
+				lapsed: 10,
+				attempts: 1,
+				solution: 'function reverse(str) { return str.split(\'\').reverse().join(\'\'); }'
+			},
+			{
+				ID: 2,
+				lapsed: 27,
+				attempts: 2,
+				solution: 'function multiply(val) { return parseInt(val, 10) * 2; }'
+			}
+		]
+	};
+
+	function getData() {
+		return JSON.parse(localStorage.getItem('assess'));
+	}
+
+	beforeEach(function () {
+		localStorage.setItem('assess', JSON.stringify(data));
+		State.init();
+	});
+
+	describe('state', function () {
+		it('should initialize', function () {
+			expect(State.data).toEqual(getData());
+		});
+
+		it('should set lapsed time', function () {
+			State.setLapsedTime(100);
+
+			expect(State.data.lapsed).toEqual(100);
+			expect(getData().lapsed).toEqual(100);
+		});
+
+		it('should get lapsed time', function () {
+			expect(State.getLapsedTime()).toEqual(37);
+		});
+
+		it('should get questions', function () {
+			expect(State.getQuestions().length).toEqual(2);
+		});
+
+		it('should set single question', function () {
+			var question = data.questions[0];
+			question.lapsed = 15;
+			State.setQuestion(question);
+
+			expect(State.data.questions[0].lapsed).toEqual(15);
+			expect(getData().questions[0].lapsed).toEqual(15);
+		});
+
+		it('should get single question', function () {
+			expect(State.getQuestion(1)).toEqual(data.questions[0]);
+		});
+	});
+});
